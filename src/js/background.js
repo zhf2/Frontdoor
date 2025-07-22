cde7acc9254ef3cdb27274cb0725bc37d63a79af
zhf2 <zhf2@student.le.ac.uk>
2025-07-22 19:46:51 +0100

    Added clock

--- src/js/background.js
+++ src/js/background.js
@@ -3,4 +3,13 @@ window.onload = function () {
     const date = new Date();
     let ms = -(date.getTime() - date.setHours(0,0,0));
     document.getElementById("body").style.animationDelay = ms + "ms";
+
+    updateClock();
+}
+
+function updateClock() {
+    const date = new Date();
+    document.getElementById("time_display").textContent =
+        date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0");
+    setInterval(updateClock, 1000);
 }
